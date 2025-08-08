71edfa26b7b0fbe9a64881f28c28a4fc88e99d28
F_Muhammad <maga.mutsolgov@bk.ru>
2025-08-09 00:36:12 +0300

    DO3_LinuxMonitoring

--- src/04/functions.sh
+++ src/04/functions.sh
@@ -38,3 +38,35 @@ get_uptime() {
     uptime -p
 }
 
+# 7. Время работы в секундах
+get_uptime_sec() {
+    cat /proc/uptime | awk '{print int($1)}'
+}
+
+# 8. Первый непустой IPV4-адрес и маска
+get_ip_and_mask() {
+    ip -4 addr show scope global | awk '/inet/ {
+    split($2,a,"/");
+    print a[1], "/" a[2];
+    exit
+    }'
+}
+
+# 9. Шлюз по умолчанию
+get_gateway() {
+    ip route | awk '/default/ {print $3; exit}'
+}
+
+# 10. Память: всего, занято, свободно
+get_ram() {
+    local total used free
+    read total used free _ < <(free --mega | awk '/Mem:/ {printf "%.3f %.3f %.3f\n", $2/1024, $3/1024, $4/1024}')
+    echo "$total" "$used" "$free"
+}
+
+# 11. Пространство в корне: всего, занято, свободно
+get_root_space() {
+    local total used free
+    read total used free _ < <(df --output=size,used,avail --block-size=1M / | tail -n1 | awk '{printf "%.2f %.2f %.2f\n", $1, $2, $3}')
+    echo "$total" "$used" "$free"
+}
