0c9b23b6be316fa7b094b53a5c3f42b8d9af1786
F_Muhammad <maga.mutsolgov@bk.ru>
2025-08-09 00:35:47 +0300

    DO3_LinuxMonitoring

---
+++ src/04/functions.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# 1. Получаем имя хоста
+get_hostname() {
+    hostname
+}
+
+# 2. Получаем временную зону: Continent/City и смещение UTC
+get_timezone() {
+    local tz_name
+    tz_name=$(cat /etc/timezone 2>/dev/null) || tz_name=$(timedatectl show -p Timezone --value)
+    local utc_offset
+    utc_offset=$(date +%:z)
+    echo "$tz_name UTC $utc_offset"
+}
+
+# 3. Получаем текущего пользователя
+get_user() {
+    whoami
+}
+
+# 4. Тип и версия OC
+get_os() {
+    if command -v lsb_release >/dev/null; then
+        lsb_release -ds
+    else
+        grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2- | tr -d '"'
+    fi
+}
+
+# 5. Текущее дата-время
+get_date() {
+    date +"%d %B %Y %T"
+}
+
+# 6. Время работы системы
+get_uptime() {
+    uptime -p
+}
+
