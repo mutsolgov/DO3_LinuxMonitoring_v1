a7dac5fac38d66b4fc5313c7d271a4e7bcef307b
F_Muhammad <maga.mutsolgov@bk.ru>
2025-08-10 03:33:33 +0300

    DO3_LinuxMonitoring

---
+++ src/04/main.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Массивы ANSI-кодов
+bg_colors=("" "\033[107m" "\033[101m" "\033[102m" "\033[104m" "\033[105m" "\033[40m")
+fg_colors=("" "\033[97m"  "\033[91m"  "\033[92m"  "\033[94m"  "\033[95m"  "\033[30m")
+reset="\033[0m"
+
+# Цвета по умолчанию
+DEFAULT_COL1_BG=6
+DEFAULT_COL1_FG=1
+DEFAULT_COL2_BG=3
+DEFAULT_COL2_FG=4
+
