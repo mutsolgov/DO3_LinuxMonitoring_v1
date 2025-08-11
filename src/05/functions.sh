5eb02e625e8146ab1a3ecff5fa1560378e5e1418
F_Muhammad <maga.mutsolgov@bk.ru>
2025-08-11 22:21:29 +0300

    DO3_LinuxMonitoring

--- src/05/functions.sh
+++ src/05/functions.sh
@@ -20,3 +20,33 @@ get_total_files() {
     find "$1" -type f | wc -l
 }
 
+# 4) Подсчет по типам
+get_count_by_ext() {
+    local dir=$1 ext_pattern=$2
+    find "$dir" -type f -iname "*.$ext_pattern" | wc -l
+}
+get_count_exe() {
+    find "$1" -type f -executable | wc -l
+}
+get_count_links() {
+    find "$1" -type l | wc -l 
+}
+get_count_text() {
+    local dir="$1"
+    find "$dir" -type f -print0 \
+        | xargs -0 -P "$(nproc)" grep -IPl . \
+        | wc -l
+}
+
+# 5) Топ-N файлов по размеру: возвращает "path, size, ext"
+get_top_files() {
+    local dir=$1 count=$2
+    find "$dir" -type f -printf "%s %p\n" \
+        | sort -nrk1,1 \
+        | head -n"$count" \
+        | while read -r size path; do
+            ext="${path##*.}"
+            echo "$path,$size,$ext"
+        done
+}
+
