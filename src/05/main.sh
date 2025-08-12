0a4ab1430de8e2baa941048ee5fb758a29a287cd
F_Muhammad <maga.mutsolgov@bk.ru>
2025-08-12 17:28:41 +0300

    DO3_LinuxMonitoring

--- src/05/main.sh
+++ src/05/main.sh
@@ -18,3 +18,42 @@ START=$(date +%s.%N)
 # 2. Подключаем функции
 source "$(dirname "$0")/functions.sh"
 
+# 3. Собираем метрики
+total_dirs=$(get_total_dirs "$DIR")
+top5=$(get_top_dirs "$DIR" 5)
+total_files=$(get_total_files "$DIR")
+
+conf_files=$(get_count_by_ext "$DIR" "conf")
+log_files=$(get_count_by_ext "$DIR" "log")
+arch_files=$(find "$DIR" -type f \( -iname '*.zip' -o -iname '*.gz' -o -iname '*.bz2' -o -iname '*.7z' \) | wc -l)
+links=$(get_count_links "$DIR")
+exe_files=$(get_count_exe "$DIR")
+text_files=$(get_count_text "$DIR")
+
+top10_files=$(get_top_files "$DIR" 10)
+top10_exe=$(get_top_exe_files "$DIR" 10)
+
+# 4. Время окончания
+END=$(date +%s.%N)
+elapsed=$(echo "$END - $START" | bc)
+
+# 5. Выводим результат
+echo "Total number of folders (including all nested ones) = $total_dirs"
+
+echo "TOP 5 folders of maximum size arranged in descending order (path and size):"
+i=1
+while IFS=',' read -r path size; do
+    echo "$i - $path, $(to_human "$size")"
+    ((i++))
+done <<<"$top5"
+echo "etc up to 5"
+
+echo "Total number of files= $total_files"
+echo "Number of:"
+echo "Configuration files (with the .conf extension) = $conf_files"
+echo "Text files = $text_files"
+echo "Executable files = $exe_files"
+echo "Log files (with the extension .log) = $log_files"
+echo "Archive files = $arch_files"
+echo "Symbolic links = $links"
+
